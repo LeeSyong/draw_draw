c1cb93878f1b5bc05d779654867ca6cd5f41bc6b
LeeSyong <jejelsk27@naver.com>
2022-07-07 15:42:38 +0900

    Feat: window resize event 등록
    - (브라우저 창과 크기가 동일한) 캔버스의 크기 및 비율 조정 목적

--- src/canvas/Drawing.js
+++ src/canvas/Drawing.js
@@ -3,6 +3,8 @@ class DrawingCanvas {
     this._canvas = canvas;
     this._ctx = this._canvas.getContext("2d");
 
+    this._resize();
+
     this._canvas.addEventListener("mousedown", (event) => this._engage(event));
     this._canvas.addEventListener("mousemove", (event) => this._drawXY(event)); // 그림 쪽에서는 putPoints
     this._canvas.addEventListener("mouseup", (event) => this._disengage(event));
@@ -21,7 +23,21 @@ class DrawingCanvas {
 
   _disengage(event) {}
 
-  _resize() {}
+  _resize() {
+    const displayWidth = this._canvas.clientWidth;
+    const displayHeight = this._canvas.clientHeight;
+
+    const needResize =
+      this._canvas.width !== displayWidth ||
+      this._canvas.height !== displayHeight;
+
+    if (needResize) {
+      this._canvas.width = displayWidth;
+      this._canvas.height = displayHeight;
+    }
+
+    return needResize;
+  }
 }
 
 export default DrawingCanvas;
