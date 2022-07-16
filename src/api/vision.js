e7ad906f410f2cad2d3f72a0d45d31c9fdf5ee00
LeeSyong <jejelsk27@naver.com>
2022-07-16 14:49:06 +0900

    Feat: Vision API 로직 작성
    - 사용자가 그린 글자를 인식하기 위해 Vision API 이용
    - api/vision.js 파일에 관련 로직 작성

---
+++ src/api/vision.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+import { API } from "../constants/url";
+import { TEXT } from "../constants/text";
+
+import ui from "../utils/ui";
+
+const vision = (() => {
+  const recognize = async (canvas) => {
+    try {
+      const result = await axios.post(
+        `${API.TEXT_ENDPOINT}?key=${process.env.API_KEY}`,
+        {
+          requests: [
+            {
+              image: {
+                content: canvas
+                  .toDataURL()
+                  .replace(/^data:image\/[a-z]+;base64,/, ""),
+              },
+              features: [
+                {
+                  type: "TEXT_DETECTION",
+                  maxResults: 1,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      const text = result.data.responses[0].fullTextAnnotation?.text;
+
+      if (!text) {
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        ui.addText(TEXT.DRAW_LETTER_ERROR);
+      }
+
+      return text;
+    } catch (error) {
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      ui.addText(TEXT.DRAW_LETTER_ERROR);
+    }
+  };
+
+  return Object.freeze({ recognize });
+})();
+
+export default vision;
--- src/constants/text.js
+++ src/constants/text.js
@@ -1,6 +1,7 @@
 const TEXT = {
   DRAW_PICTURE: "그림을 그리세요",
   DRAW_LETTER: "글자를 그리세요",
+  DRAW_LETTER_ERROR: "글자를 다시 그려주세요",
 };
 
 export { TEXT };
